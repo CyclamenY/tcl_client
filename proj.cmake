959f6c57e1c37e1c39068a4e3515691123c0f4ea
CyclamenY <angryyzh@hotmail.com>
2025-01-22 22:46:32 +0800

    modify action file

--- proj.cmake
+++ proj.cmake
@@ -1,10 +1,18 @@
 # define var
-set(CMAKE_PREFIX_PATH "F:/software/Qt/msvc2022_x64")
+if(WORKFLOW STREQUAL "github")
+  message(STATUS ${QT_ROOT_DIR})
+  set(CMAKE_PREFIX_PATH "${QT_ROOT_DIR}")
+  set(MY_PATH "")
+  set(QT_QPA_PLATFORM_PLUGIN_PATH "")
+else()
+  set(CMAKE_PREFIX_PATH "F:/software/Qt/msvc2022_x64")
+  set(MY_PATH "F:/software/Qt/msvc2022_x64/bin;F:/software/tcl8.6.15/bin")
+  set(QT_QPA_PLATFORM_PLUGIN_PATH "F:/software/Qt/msvc2022_x64/plugins/platforms")
+endif()
+
 set(QT_MODULES "Widgets" "Core")
 set(QT_DLLS "Widgets" "Gui" "Core")
 set(REF_LIB_TOP_PATH ${CMAKE_SOURCE_DIR}/ref/${ARCH}/lib)
-set(MY_PATH "F:/software/Qt/msvc2022_x64/bin;F:/software/tcl8.6.15/bin")
-set(QT_QPA_PLATFORM_PLUGIN_PATH "F:/software/Qt/msvc2022_x64/plugins/platforms")
 
 # define marco
 macro(get_git_hash _git_hash)
